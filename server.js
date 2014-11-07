6f1d286b7d16ef785f52b01a13b9eb06e2ab58f1
Cahlan Sharp <cahlan@gmail.com>
2014-11-07 14:20:18 -0700

    adds logs and removes node_modules

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 var express = require('express')
+var passport = require('passport');
+
 
 var app = express();
 app.use(express.static(__dirname+'/public'));
 
+console.log(process.env);
+console.log("current port", process.env.EXPRESS_PORT || 8888);
 
-app.listen(process.env.NODE_PORT || 8888);
+app.post('/test', function(req, res) {
+    console.log("POST to the /test endpoint");
+    return res.status(200).end(); 
+});
+
+app.listen(process.env.EXPRESS_PORT || 8888);
